bb30de64545923394b078c5e6aaadaed1900dbba
Jim Challenger <jchallenger@groupon.com>
2017-03-29 12:27:27 -0500

    Add increasing number

--- src/ui/components/glimmer-weather/component.ts
+++ src/ui/components/glimmer-weather/component.ts
@@ -1,5 +1,14 @@
-import Component from "@glimmer/component";
+import Component, {tracked} from "@glimmer/component";
 
 export default class GlimmerWeather extends Component {
+  @tracked temp = 50;
 
+  constructor(options) {
+    super(options);
+    this.loadWeather();
+  }
+
+  loadWeather() {
+     setInterval(() => { this.temp = this.temp + 1 }, 500)
+  }
 }
